8cbdbbe0bfbb6babdc192edcfc86da8d66c5a96f
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-07-24 21:41:17 +0300

    Two cameras detection

--- object_two_obj.py
+++ object_two_obj.py
@@ -1,13 +1,12 @@
 import cv2
+import imutils
+import time
 
 #img = cv2.imread('person.jpg')
 threshold=0.5
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-
 classNames=[]
 classFile='model_data/coco.names'
+
 with open(classFile, 'rt') as f:
     classNames=f.read().rstrip('\n').split('\n')
 #print(classNames)
@@ -22,9 +21,15 @@ net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5,127.5,127.5))
 net.setInputSwapRB(True)
 
-count =0;
-while True:
-    success, img =cap.read()
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+cap1 = cv2.VideoCapture(1)
+cap1.set(3,640)
+cap1.set(4,480)
+
+
+def show_img0(img):
     classIds, confs, bbox = net.detect(img,confThreshold=threshold)
     #print(classIds,bbox)
     if len(classIds) != 0:
@@ -34,10 +39,34 @@ while True:
             print(classNames[classId-1])
             cv2.putText(img,str(confidence),(box[0]+10,box[1]+70), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2) 
             print(classNames[classId-1])
-            count =count+ 1
-            #print(count)
+
+def show_img1(img):
+    classIds, confs, bbox = net.detect(img,confThreshold=threshold)
+    #print(classIds,bbox)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0,255,0), thickness=2)
+            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2) 
+            print(classNames[classId-1])
+            cv2.putText(img,str(confidence),(box[0]+10,box[1]+70), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2) 
+            print(classNames[classId-1])
             
-      
-    cv2.imshow("output",img)
+while True:
+    #success, img =cap.read()
+    ret0, img = cap.read()
+    assert ret0 # succeeds
+    # success, img1 =cap1.read()
+    ret1, img1 = cap1.read()
+    assert ret1 # fails?!
+    
+    show_img0(img)
+    show_img1(img1)
+    cv2.imshow("output 1",img)
+    cv2.imshow("output 2",img1)
     cv2.waitKey(1)
+    
+    
+   
+    
+    
 
